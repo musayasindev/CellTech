ac02468369b65b01753e4b2d3c4672f1314430ca
khizaryasin <mkhizaryasin123@gmail.com>
2022-02-09 22:32:38 +0500

    wepromise

--- celtech.js
+++ celtech.js
@@ -1,1 +1,8 @@
+let navbar = document.getElementById('navbar')
 
+$(window).scroll(function (){
+    let scrollDown = $(document).scrollTop()
+    if(scrollDown > 200){
+        $("#navbar").css("background-color", "black");
+    }
+})
